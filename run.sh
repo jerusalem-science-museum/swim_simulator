f6f39ed23e213078ef5b24ab288220f6ed36c396
shoamshilo <shoamshilo@gmail.com>
2021-08-26 05:06:58 -0700

    Added local js and css

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash 
-
+# TODO abslout path 
 # parallel  ::: "sudo python3 src/app.py" "python3 src/server.py"
 
-python3 src/server.py & python3 src/app.py
+python3 src/server.py &
+python3 src/app.py
